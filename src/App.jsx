b41d6c85ab5368bca86c5eab6c5d7b566007bbd6
Yousuf-cse <yousuf.mallik79@gmail.com>
2025-01-06 23:22:11 +0530

    Remove unused code and restructure App.jsx for better readability and efficiency

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,11 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 import '@fortawesome/fontawesome-free/css/all.min.css';
 
-
-function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-     
+    
     </>
   )
-}
+
 
 export default App
