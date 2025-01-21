777899e97b7623b7527a6e61cc6b90643e984eed
Yousuf-cse <yousuf.mallik79@gmail.com>
2025-01-21 20:53:30 +0530

    added a absolute positioning h1, fix some elements position and added a verticalgrid

--- src/Components/LoginPage/LoginPage.jsx
+++ src/Components/LoginPage/LoginPage.jsx
@@ -14,16 +14,23 @@ const LoginPage = () => {
   };
 
   return (
-    <div className="grid grid-cols-2 min-h-screen">
+    <div className="relative min-h-screen">
+      <h1 className='font-playfair absolute inset-x-0 top-12 left-60 text-center text-9xl font-light z-0'>
+          OUTFIT ORBIT
+      </h1>
+      {/* <h1 className='font-playfair absolute inset-x-0 top-44 right-36 text-center text-9xl font-light z-0'>
+          ORBIT
+      </h1> */}
+    <div className="grid grid-cols-[1fr_1fr_70px] min-h-screen">
       <div className=" items-start justify-start ">
         <img 
         src={LoginUiImg}
         alt="LoginUiImg" 
-        className='ml-16 mt-16 w-1/2 h-auto bg-pink-700'
+        className='ml-16 mt-16 w-2/3 h-auto bg-pink-700'
         />
       </div>
       <div className="flex items-center justify-center">
-  <form onSubmit={handleSubmit} className="w-full max-w-sm h-64 bg-[#d1e7ae] p-8 rounded-2xl shadow-md leading-none">
+  <form onSubmit={handleSubmit} className="w-full max-w-sm h-64 bg-[#d1e7ae] p-8 rounded-2xl shadow-md leading-none mt-16 mr-40">
     {/* <h2 className="text-2xl font-bold text-center text-gray-800 mb-6">Login</h2> */}
     <div className="mb-4">
       <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="email">
@@ -35,7 +42,7 @@ const LoginPage = () => {
         value={Number}
         onChange={(e) => setNumber(e.target.value)}
         required
-        className="appearance-none border-b border-black w-1/2 py-2 px-2 leading-tight focus:outline-none focus:ring-0 focus:border-purple-500 bg-[#d1e7ae]"
+        className="appearance-none border-b border-black w-1/2 py-2 px-2 leading-tight focus:outline-none focus:ring-0 focus:border-[#6495ED] bg-[#d1e7ae]"
         // placeholder="Enter your mobile number"
       />
     </div>
@@ -49,7 +56,7 @@ const LoginPage = () => {
         value={password}
         onChange={(e) => setPassword(e.target.value)}
         required
-        className="appearance-none border-b border-black w-1/2 py-2 px-2 leading-tight focus:outline-none focus:ring-0 focus:border-purple-500 bg-[#d1e7ae]"
+        className="appearance-none border-b border-black w-1/2 py-2 px-2 leading-tight focus:outline-none focus:ring-0 focus:border-[#6495ED] bg-[#d1e7ae]"
         // placeholder="********"
       />
     </div>
@@ -69,52 +76,9 @@ const LoginPage = () => {
     </p>
   </form>
 </div>
-
-          
-        {/* <h2 className="text-2xl font-bold text-center text-gray-800 mb-6">Login to Outfit Orbit</h2>
-        <form onSubmit={handleSubmit}>
-          <div className="mb-4">
-            <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="email">
-              Email
-            </label>
-            <input
-              type="email"
-              id="email"
-              value={email}
-              onChange={(e) => setEmail(e.target.value)}
-              required
-              className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-              placeholder="you@example.com"
-            />
-          </div>
-          <div className="mb-6">
-            <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="password">
-              Password
-            </label>
-            <input
-              type="password"
-              id="password"
-              value={password}
-              onChange={(e) => setPassword(e.target.value)}
-              required
-              className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-              placeholder="********"
-            />
-          </div>
-          <div className="flex items-center justify-between">
-            <button
-              type="submit"
-              className="bg-purple-600 hover:bg-purple-500 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
-            >
-              Login
-            </button>
-          </div>
-          <p className="text-center text-gray-600 text-xs mt-4">
-            Don't have an account? <a href="/register" className="text-purple-600 hover:text-purple-500">Sign Up</a>
-          </p>
-        </form> */}
-      {/* </div> */}
-    </div>
+<div className="bg-[#d1e7ae] w-full"></div>
+</div>
+</div>
   );
 };
 
